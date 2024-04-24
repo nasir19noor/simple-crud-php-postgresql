0c6230a45b99c43681c3bfd97d1438424c9cc96a
Nasir Nooruddin <nasir@nasir.id>
2024-04-24 19:15:59 +0700

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -3,19 +3,18 @@
 --
 
 CREATE TABLE IF NOT EXISTS employee (
-  id serial NOT NULL COMMENT primary key,
-  employee_name varchar(255) NOT NULL COMMENT employee name,
-  employee_salary double NOT NULL COMMENT employee salary,
-  employee_age int(11) NOT NULL COMMENT employee age,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='datatable demo table' AUTO_INCREMENT=64 ;
+  id serial NOT NULL primary key,
+  employee_name varchar(255) NOT NULL,
+  employee_salary float8 NOT NULL,
+  employee_age int NOT NULL
+);
 
 --
 -- Dumping data for table `employee`
 --
 
 INSERT INTO employee (id, employee_name, employee_salary, employee_age) VALUES
-(1, 'Nasir Nooruddin', 1000000, 40),
+(1, 'John Wick', 1000000, 40),
 (2, 'Joko Purba', 680000, 51),
 (3, 'Putu Suarsana', 300000, 45);
 
